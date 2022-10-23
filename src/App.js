f663200e1b10cb66d4cefbf586cc94eef25b3803
madstodamoon <108613671+madstodamoon@users.noreply.github.com>
2022-10-23 00:57:58 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div class="vimeo-wrapper">
+      <iframe
+        src="https://player.vimeo.com/video/763046666?h=74ea60f1d7&amp;badge=0&amp;background=1&autoplay=1&loop=1&byline=0&title=0"
+        frameborder="0"
+        webkitallowfullscreen
+        mozallowfullscreen
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
+}
